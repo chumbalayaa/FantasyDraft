d79efeecdc6293bc3eb50e9f586f8a296ce450cf
Alex Chumbley <chumbleya@gmail.com>
2014-07-30 11:21:20 -0400

    Added file for ranking players and added comments

---
+++ projectionRanking.js
@@ -0,0 +1,8 @@
+//This file will take in a JSON object of all of
+//the players. It will then calculate (given a
+//set of rules in the POST request parameter)
+//their new projected fantasy points for the season.
+//Then, it will calculate their fantasyPositionIndex (FPI)
+//which will be used to determine their final ranking. 
+//These new attributes will simply be added to the existing
+//JSON object that is inputted. 
