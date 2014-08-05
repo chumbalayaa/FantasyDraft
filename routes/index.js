2da6057e4c0bbe72b8fedb381812893524dfc5c9
Karson Ota <klota@mit.edu>
2014-08-05 10:06:19 -0400

    working first page

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get('/draft', function(req, res) {
 
 /* POST to draft page. Render a draft page with custom projection*/
 router.post('/draft', function(req, res) {
-	console.log(req)
+	console.log(req);
     res.send('This should do something');
 });
 
